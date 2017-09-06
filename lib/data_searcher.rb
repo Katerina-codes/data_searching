38401034165715239e8dd437c6c42283920b378e
Katerina <katerinaldg@gmail.com>
2017-09-06 14:07:38 +0100

    Prompts user for input

--- lib/data_searcher.rb
+++ lib/data_searcher.rb
@@ -1,11 +1,16 @@
 class DataSearcher
 
-  def initialize(input = $stdin)
+  def initialize(output = $STDOUT, input = $STDIN)
+    @output = output
     @input = input
   end
 
   def get_input
     @input.gets.chomp
+  end
+
+  def ask_user_for_input
+    @output.puts "Please enter some text. We'll use this to search our records:"
   end
 
 end
--- spec/data_searcher_spec.rb
+++ spec/data_searcher_spec.rb
@@ -3,9 +3,17 @@ require 'data_searcher'
 RSpec.describe DataSearcher do
 
   it 'Gets input from the user' do
-    input = StringIO.new('Z')
-    data_searcher = DataSearcher.new(input)
-    expect(data_searcher.get_input).to eq('Z')
+    output = StringIO.new
+    input = StringIO.new("Z")
+    data_searcher = DataSearcher.new(output, input)
+    expect(data_searcher.get_input).to eq("Z")
+  end
+
+  it 'prompt the user for input' do
+    output = StringIO.new
+    data_searcher = DataSearcher.new(output)
+    data_searcher.ask_user_for_input
+    expect(output.string).to eq("Please enter some text. We'll use this to search our records:\n")
   end
 
 end
