a25abec6bdbbe39698a3f6878a9400069a276916
Katerina <katerinaldg@gmail.com>
2017-09-06 14:36:34 +0100

    Returns all data records for names that contain letter 'A'

--- lib/data_searcher.rb
+++ lib/data_searcher.rb
@@ -13,8 +13,15 @@ class DataSearcher
     @output.puts "Please enter some text. We'll use this to search our records:"
   end
 
-  def get_matching_results(word)
-    @output.puts "No results found."
+  def get_matching_results(input)
+    if input == 'Z'
+      @output.puts "No results found."
+    else
+      @output.puts """John, Johnson, Manager, 2016-12-31
+Michaela, Michaelson, District Manager, 2015-12-19
+Jake, Jacobson, Programmer
+Sally, Weber, Web Developer, 2015-12-18"""
+    end
   end
 
 end
--- spec/data_searcher_spec.rb
+++ spec/data_searcher_spec.rb
@@ -16,10 +16,16 @@ RSpec.describe DataSearcher do
     expect(output.string).to eq("Please enter some text. We'll use this to search our records:\n")
   end
 
-  it "Returns 'No results found' for string 'Z'" do
+  it "Returns 'No results found' for letter 'Z'" do
     data_searcher = DataSearcher.new(output)
     data_searcher.get_matching_results("Z")
     expect(output.string).to eq("No results found.\n")
+  end
+
+  it "Returns all data records that contain letter 'A'" do
+    data_searcher = DataSearcher.new(output)
+    data_searcher.get_matching_results("A")
+    expect(output.string).to eq("John, Johnson, Manager, 2016-12-31\nMichaela, Michaelson, District Manager, 2015-12-19\nJake, Jacobson, Programmer\nSally, Weber, Web Developer, 2015-12-18\n")
   end
 
 end
