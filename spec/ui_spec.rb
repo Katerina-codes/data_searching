07ac4ab99f8009eb779d72040519965fa5b81134
Katerina <katerinaldg@gmail.com>
2017-09-07 14:33:43 +0100

    Refactored tests by using let and method to remove duplication

--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -1,26 +1,24 @@
 require 'ui'
 
 RSpec.describe UI do  
-  
+  let(:output) { StringIO.new }
+ 
   it 'prompts the user for input' do
-    output = StringIO.new
-    ui = UI.new(output)
-    ui.ask_user_for_input
+    create_ui.ask_user_for_input
     expect(output.string).to eq("Please enter some text. We'll use this to search our records:\n")
   end
  
   it 'gets input from the user' do
-    output = StringIO.new
-    input = StringIO.new("Z")
-    ui = UI.new(output, input)
+    ui = create_ui(StringIO.new("Z"))
     expect(ui.get_input).to eq("Z")
   end
 
   it "displays message when there's no results" do
-    output = StringIO.new
-    ui = UI.new(output)
-    ui.display_no_results_message
+    create_ui.display_no_results_message
     expect(output.string).to eq("No results found.\n")
   end
 
+ def create_ui(input = StringIO.new)
+    UI.new(output, input)
+ end
 end
