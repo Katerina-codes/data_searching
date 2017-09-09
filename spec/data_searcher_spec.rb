20ec742e33e81006a7fc1ff781672c9ca0694891
Katerina <katerinaldg@gmail.com>
2017-09-09 17:54:08 +0100

    returns all data records for names that contain 'ae'

--- spec/data_searcher_spec.rb
+++ spec/data_searcher_spec.rb
@@ -21,7 +21,12 @@ RSpec.describe DataSearcher do
     expect(data_searcher.get_matching_results("C")).to eq([['Jacquelyn', 'Jackson', 'DBA'], ['Jake', 'Jacobson', 'Programmer'], ['Michaela', 'Michaelson', 'District Manager', '2015-12-19']])
   end
 
-  def get_data_searcher(ui)
+  it "Returns all data records for names that contain 'ae'" do
+    data_searcher = get_data_searcher(ui)
+    expect(data_searcher.get_matching_results("ae")).to eq([['Michaela', 'Michaelson', 'District Manager', '2015-12-19']])
+  end
+
+  def get_data_searcher(ui = UI.new)
     DataSearcher.new(ui)
   end
 
