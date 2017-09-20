78b6d35b813e9653f24e32368b5923d02a214d8a
Katerina <katerinaldg@gmail.com>
2017-09-20 11:32:26 +0100

    Corrected indentation

--- lib/record_manager.rb
+++ lib/record_manager.rb
@@ -8,7 +8,7 @@ class RecordManager
 
   def create_data_hash(records)
     records.split("\n").map do |record|
-    record = record.split(",")
+      record = record.split(",")
       Hash[:first_name, record[0], :last_name, record[1], :role, record[2], :date, record[3]]
     end
   end
