dd47ec34c961c71115786e336a3331c8938c51e1
Katerina <katerinaldg@gmail.com>
2017-09-21 12:04:12 +0100

    Changed hash syntax to be more explicit

--- lib/record_manager.rb
+++ lib/record_manager.rb
@@ -9,7 +9,12 @@ class RecordManager
   def create_data_hash(records)
     records.split("\n").map do |record|
       record = record.split(",")
-      Hash[:first_name, record[0], :last_name, record[1], :role, record[2], :date, record[3]]
+      {
+        first_name: record[0],
+        last_name: record[1],
+        role: record[2],
+        date: record[3]
+      }
     end
   end
 end
