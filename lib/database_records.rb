136c329716c8606bc1a4ead07daa6ffbddb6bed0
Katerina <katerinaldg@gmail.com>
2017-10-05 14:10:03 +0100

    Initialized DatabaseRecords with test database.

--- lib/database_records.rb
+++ lib/database_records.rb
@@ -1,6 +1,10 @@
 class DatabaseRecords
 
+  def initialize(database)
+    @database = database
+  end
+
   def access_records
-    []
+    @database[:employee].all
   end
 end
--- spec/database_records_spec.rb
+++ spec/database_records_spec.rb
@@ -1,9 +1,11 @@
 require 'database_records'
+require 'sequel'
 
 RSpec.describe DatabaseRecords do
 
   it "returns empty list when there is no records" do
-    database_records = DatabaseRecords.new
+    DB = Sequel.connect('mysql2://test-user:coolbeans@localhost:3306/test_database')
+    database_records = DatabaseRecords.new(DB)
 
     expect(database_records.access_records).to eq([])
   end
