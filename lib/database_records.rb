fda45d3014972a010cf8f4225d82ac426773ab2a
Katerina <katerinaldg@gmail.com>
2017-10-06 09:43:29 +0100

    Shortened method names

--- lib/database_records.rb
+++ lib/database_records.rb
@@ -4,16 +4,16 @@ class DatabaseRecords
     @database = database
   end
 
-  def access_records
+  def access
     @database[:employee].all
   end
 
-  def write_to_records(record)
+  def write_to(record)
     each_record_value = record.split(",")
     @database[:employee].insert(first_name: each_record_value[0], last_name: each_record_value[1], role: each_record_value[2], end_of_employment: nil)
   end
 
-  def delete_all_records
+  def delete_all
     @database[:employee].delete
   end
 end
