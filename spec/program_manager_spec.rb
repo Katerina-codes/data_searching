a57f94cf842f4dd800d1ba71f98bcdaef269fb01
Katerina <katerinaldg@gmail.com>
2017-10-06 12:06:45 +0100

    Extracted duplicated logic to a new method

--- spec/program_manager_spec.rb
+++ spec/program_manager_spec.rb
@@ -2,20 +2,19 @@ require 'program_manager'
 require 'ui'
 require 'data_searcher'
 require 'table'
-require 'record_manager'
+require 'database_records'
 
 RSpec.describe ProgramManager do
 
   let(:ui) { instance_double('UI') }
   let(:table) { instance_double('Table') }
   let(:data_searcher) { instance_double('DataSearcher') }
-  let(:record_manager) { instance_double('RecordManager') }
-
+  let(:record_manager) { instance_double('DatabaseRecords') }
   it 'User provides the search criteria' do
-    program_manager = ProgramManager.new(ui, data_searcher, table, record_manager)
+    program_manager = program_manager_instance
     allow(ui).to receive(:get_user_intention).and_return('1')
     allow(ui).to receive(:ask_user_for_input)
-    allow(record_manager).to receive(:access_records)
+    allow(record_manager).to receive(:access)
     allow(record_manager).to receive(:create_data_hash)
     allow(ui).to receive(:get_valid_input)
     allow(data_searcher).to receive(:get_matching_results)
@@ -30,7 +29,7 @@ RSpec.describe ProgramManager do
     input = StringIO.new("1\n1\nx")
     output = StringIO.new
     ui_with_input = UI.new(output, input)
-		program_manager = ProgramManager.new(ui_with_input, DataSearcher.new, Table.new, RecordManager.new('spec/test_file.txt'))
+    program_manager = ProgramManager.new(ui_with_input, DataSearcher.new, Table.new, RecordManager.new('spec/test_file.txt'))
 
     program_manager.user_flow
 
@@ -39,9 +38,9 @@ RSpec.describe ProgramManager do
   end
 
   it 'asks the user for their intention' do
-    program_manager = ProgramManager.new(ui, data_searcher, table, record_manager)
+    program_manager = program_manager_instance
     allow(ui).to receive(:get_search_criteria)
-    allow(record_manager).to receive(:access_records)
+    allow(record_manager).to receive(:access)
     allow(record_manager).to receive(:create_data_hash)
     allow(data_searcher).to receive(:get_matching_results)
     allow(ui).to receive(:format_results)
@@ -52,14 +51,17 @@ RSpec.describe ProgramManager do
   end
 
   it 'adds a new record to the records file' do
-    program_manager = ProgramManager.new(ui, data_searcher, table, record_manager)
+    program_manager = program_manager_instance
     allow(ui).to receive(:get_user_intention)
     allow(ui).to receive(:get_search_criteria)
     allow(ui).to receive(:get_user_record)
 
-    expect(record_manager).to receive(:write_to_records)
+    expect(record_manager).to receive(:write_to)
 
     program_manager.user_flow
   end
 
+  def program_manager_instance
+    program_manager = ProgramManager.new(ui, data_searcher, table, record_manager)
+  end
 end
